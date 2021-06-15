cedef0204ec17c2d9586f05ab98bd48cd8a64506
Jisoo <helloworld.jisoo@gmail.com>
2021-06-15 10:34:36 +0100

    debug

---
+++ debug/app.js
@@ -0,0 +1,22 @@
+function sayhello(){
+  console.log("hello🙋‍♀️")
+}
+
+function calculate(x,y){
+  console.log('calculating...');
+  const result = x+y;
+  console.log('result: ' + result);
+  sayhello();
+  return result;
+}
+
+calculate(1,2);
+
+const stop = 4;
+console.log('......👉looping👉.....');
+for(let i = 0; i<10; i++){
+console.log('count',i);
+if(i===stop){
+  break;
+}
+}
