978c07c1ca3565aee26fce794701a1a76623c4b9
Jisoo <helloworld.jisoo@gmail.com>
2021-06-29 15:24:27 +0100

    error-handling and route

---
+++ 6-express/app-route.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import postRouter from './router/post.js';
+import userRouter from './router/user.js';
+
+const app = express();
+
+// 내부 middleware
+app.use(express.json()); // REST API -> Body
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body
+
+const options = {
+  dotfiles: 'ignore',
+  etag: false,
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  },
+};
+app.use(express.static('public', options));
+
+app.use('/posts', postRouter);
+app.use('/users', userRouter);
+
+app.listen(8080);
--- 6-express/app.js
+++ 6-express/app.js
@@ -1,9 +1,23 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
-app.post('/', (req, res, next)=>{
-  console.log(req.body);
-});
 
+app
+.route('/posts')
+.get((req, res, next)=>{
+  res.status(201).send('GET: /posts');
+})
+.post((req, res, next)=>{
+  res.status(201).send('POST: /posts');
+})
+
+app
+.route('/posts/:id')
+.put((req, res, next)=>{
+  res.status(201).send('PUT: /posts/:id');
+})
+.delete((req, res, next)=>{
+  res.status(201).send('DELETE: /posts/:id');
+})
 app.listen(8080);
 
