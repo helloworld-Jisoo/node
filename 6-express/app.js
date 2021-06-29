d66df3074f6a4bee920734352b8ddfdf9882c9ad
Jisoo <helloworld.jisoo@gmail.com>
2021-06-29 13:36:43 +0100

    middleware/post

--- 6-express/app.js
+++ 6-express/app.js
@@ -1,40 +1,9 @@
 import express from 'express';
 const app = express();
-
-app.all('/api/*',(req, res, next)=> {
-  console.log('all');
-  next();
-})
-app.use('/sky',(req, res, next)=> {
-  console.log('use');
-  next();
-})
-
-app.get(
-  '/', 
-  (req, res, next)=> {
-  console.log('first');
-  if(true){
-    return res.send('Hello');
-  }
-  res.send('Jisoo');
-},
- (req, res, next)=> {
-  console.log('first-2');
-
-})
-
-app.get('/', (req,res,next)=> {
-  console.log('second');
+app.use(express.json());
+app.post('/', (req, res, next)=>{
+  console.log(req.body);
 });
 
-app.use((req,res,next)=>{
-  res.status(404).send('Not available@_@');
-})
-
-app.use((error,req,res,next) => {
-  console.log(error);
-  res.status(500).send('Sorry, try later!');
-})
 app.listen(8080);
 
